82ee4891b7e3ca1fce693952f60bd2f93c2cb8d2
abdullah <abdullah.work06@gmail.com>
2022-07-31 06:36:18 +0200

    adding cars to view

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -7,16 +7,18 @@
         </div>
 
         <div class="w-5/6 py-10">
+            @foreach ($cars as $car)
             <div class="m-auto">
                 <span class="uppercase text-blue-500 font-bold text-xs italic">
-                    Founded 2020
+                    Founded: {{ $car->founded }}
                 </span>
-            <h2 class="text-gray-700 text-5xl ">Audi</h2>
+            <h2 class="text-gray-700 text-5xl ">{{ $car->name }}</h2>
 
-            <p class="text-lg text-gray-700 py-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam iure sit eum! Distinctio nulla a iure sequi quae at explicabo doloremque quas aliquam veniam harum, nesciunt architecto, molestiae dolores maxime.</p>
+            <p class="text-lg text-gray-700 py-6">{{ $car->description }}</p>
 
             <hr class="mt-4 mb-8" >
         </div>
+            @endforeach
     </div>
 </div>
 
